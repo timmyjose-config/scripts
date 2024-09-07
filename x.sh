f2dbe70c597acc98542ec12c2afeebe6c73daad1
Timmy Jose <zoltan.jose@gmail.com>
2024-09-07 13:25:22 +0530

    Create x.sh
    Script for LeetCode and Competitive Programming.

---
+++ x.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+SRC="$1"
+OUT="${SRC%.*}"
+EXT="${SRC##*.}"
+INPUT="$2"
+
+case ${EXT} in
+  cpp | c++ | cxx)
+    g++ -Wall -Werror -std=c++2a -o ${OUT} ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      ./${OUT} < ${INPUT}
+    else
+      ./${OUT}
+    fi
+
+    rm -f ${OUT}
+    ;;
+
+  java)
+    javac ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      java -cp . ${OUT} < ${INPUT}
+    else
+      java -cp . ${OUT}
+    fi
+
+    rm -f "${OUT}.class"
+
+    ;;
+
+  rs)
+    rustc -O ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      ./${OUT} < ${INPUT}
+    else
+      ./${OUT}
+    fi
+
+    rm -f ${OUT}
+    ;;
+
+  py)
+    if [[ -f "${INPUT}" ]]; then
+      python3 ${SRC} < ${INPUT}
+    else
+      python3 ${SRC}
+    fi
+    ;;
+
+  *)
+    echo "Unsupported file type: ${EXTENSION}"
+    ;;
+esac
