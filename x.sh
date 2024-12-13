cd4567d9847ddcb4e23e6c6462e75bce8b6e5248
Timmy Jose <zoltan.jose@gmail.com>
2024-12-13 10:44:48 +0530

    Initial commit

---
+++ x.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+SRC="$1"
+shift
+
+OUT="${SRC%.*}"
+EXT="${SRC##*.}"
+INPUT="${SRC%.*}.in"
+FLAG="$@"
+
+case ${EXT} in
+  swift)
+    swiftc ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      ./${OUT} < ${INPUT}
+    else
+      ./${OUT}
+    fi
+
+    if [[ "${FLAG}" != *"--no-clean"* ]]; then
+      rm -f ${OUT}
+    fi
+    ;;
+
+  c)
+    gcc -Wall -Werror -std=c11 -o ${OUT} ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      ./${OUT} < ${INPUT}
+    else
+      ./${OUT}
+    fi
+
+    if [[ "${FLAG}" != *"--no-clean"* ]]; then
+      rm -f ${OUT}
+    fi
+    ;;
+
+  cpp | c++ | cxx)
+    g++ -Wall -Werror -std=c++2a -o ${OUT} ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      ./${OUT} < ${INPUT}
+    else
+      ./${OUT}
+    fi
+
+    if [[ "${FLAG}" != *"--no-clean"* ]]; then
+      rm -f ${OUT}
+    fi
+    ;;
+
+  java)
+    javac ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      java -cp . ${OUT} < ${INPUT}
+    else
+      java -cp . ${OUT}
+    fi
+
+    if [[ "${FLAG}" != *"--no-clean"* ]]; then
+      rm -f "${OUT}.class"
+    fi
+
+    ;;
+
+  rs)
+    rustc -O ${SRC}
+
+    if [[ -f "${INPUT}" ]]; then
+      ./${OUT} < ${INPUT}
+    else
+      ./${OUT}
+    fi
+
+    if [[ "${FLAG}" != *"--no-clean"* ]]; then
+      rm -f ${OUT}
+    fi
+    ;;
+
+  py)
+    if [[ -f "${INPUT}" ]]; then
+      python3 ${SRC} < ${INPUT}
+    else
+      python3 ${SRC}
+    fi
+    ;;
+
+  *)
+    echo "Unsupported file type: ${EXTENSION}"
+    ;;
+esac
